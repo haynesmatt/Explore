9ecf0ebe98945dd4d947395e90a8102002270716
amit-tzadok <amit.tzadok@icloud.com>
2022-04-13 20:00:52 -0400

    created Log In and Feed View Controllers

---
+++ insteadOfLab6/LoginViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LoginViewController.swift
+//  insteadOfLab6
+//
+//  Created by Amit Tzadok on 4/13/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    @IBOutlet weak var onLogIn: UIButton!
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
