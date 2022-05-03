8f2869d33a7b6ceff69d91ff5d98e1d6860197fc
Matt <matthaynes412@gmail.com>
2022-05-02 22:18:15 -0400

    Display Posts on Feed (w/o img)

--- insteadOfLab6/FeedViewController.swift
+++ insteadOfLab6/FeedViewController.swift
@@ -6,15 +6,64 @@
 //
 
 import UIKit
+import AlamofireImage
+import Parse
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var posts = [PFObject]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.collectionView.reloadData()
+            }
+        }
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print(posts.count)
+        return posts.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PostCell", for: indexPath) as! PostCell
+        
+        let post = posts[indexPath.row]
+        
+        cell.titleLabel.text = post["title"] as! String
+        print("hi")
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.photoView.af_setImage(withURL: url)
+        
+        return cell
+    }
+    
+    // "https://picsum.photos/id/237/200/300"
+    // when urlString on line 57 is replaced with above url, the image is displayed
+    // meaning there is a problem with the url of uploaded images?
 
     /*
     // MARK: - Navigation
---
+++ insteadOfLab6/PostCell.swift
@@ -0,0 +1,15 @@
+//
+//  PostCell.swift
+//  insteadOfLab6
+//
+//  Created by Matt Haynes on 5/2/22.
+//
+
+import UIKit
+import AlamofireImage
+
+class PostCell: UICollectionViewCell {
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
